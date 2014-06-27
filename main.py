4a1cbd926749003cce855d21f5d0dfe2fbe73306
Arti Zirk <arti.zirk@gmail.com>
2014-06-27 17:03:37 +0300

    it now does json output when /json path is requested

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ html = """
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        
+
         <br>
         <h1>HTTP ERROR {error_code}</h1>
         <p>{error_message}<!--<a href="http://arti.ee/">Go back</a>--></p>
@@ -35,18 +35,22 @@ f = open("7xx-errors.json", "rb")
 errors = json.loads(f.read().decode())
 f.close()
 
+
 def application(env, start_response):
     if env["PATH_INFO"] == "/favicon.ico":
-        start_response('200 OK', [('Content-Type','image/x-icon')])
+        start_response('200 OK', [('Content-Type', 'image/x-icon')])
         return [base64.b64decode(favicon)]
-    start_response('200 OK', [('Content-Type','text/html')])
     message = random.choice(errors)
+    if env["PATH_INFO"] == "/json":
+        start_response("200 OK", [('Content-Type', 'application/json')])
+        return [json.dumps({"error_code": message[0], "error_message": message[1]}).encode()]
+    start_response('200 OK', [('Content-Type', 'text/html')])
     return [html.format(error_code=message[0], error_message=message[1]).encode()]
 
 
 if __name__ == "__main__":
     from wsgiref.simple_server import make_server
-    
+
     httpd = make_server('localhost', 8080, application)
     print("Serving on http://localhost:8080/")
     httpd.serve_forever()
